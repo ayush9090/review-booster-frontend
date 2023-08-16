bea2ad9fa63caa30136838ae8b8d8f0f112d9909
ayush <Avariyava@gmail.com>
2023-08-16 00:06:47 -0500

    add contact us

--- src/features/ContactUs/ContactUs.tsx
+++ src/features/ContactUs/ContactUs.tsx
@@ -1,10 +1,30 @@
-import React, { useState } from "react";
-
-const ContactUs: React.FC = () => {
+import React, { FormEvent, useContext, useEffect, useState } from "react";
+import logo from "../assets/logos/Logo.png";
+import { AuthContext } from "../../context/AuthWrapper";
+import { useNavigate } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
+import {
+  Container,
+  Items,
+  Input,
+  SendreviewButton
+} from "./ContactUsStyles";
+const ContactUs: React.FC<{}> = () => {
+  const isSmallScreen = useMediaQuery({
+    query: " (max-width: 721px)",
+  });
+  console.log(isSmallScreen);
   return (
-    <>
-      <h1>In Contac tUs</h1>
-    </>
+    <Container>
+      <Items smallscreen={isSmallScreen}>
+         <h2>contact Us</h2>
+        <Input type="text" placeholder="Enter full name"></Input>
+        <Input type="email" placeholder="Enter your email"></Input>
+        <Input type="text" style={{height:"10rem"}} placeholder="Message"></Input>
+
+        <SendreviewButton>Send</SendreviewButton>
+      </Items>
+    </Container>
   );
 };
 
--- src/features/ContactUs/ContactUsStyles.tsx
+++ src/features/ContactUs/ContactUsStyles.tsx
@@ -1,9 +1,51 @@
 import styled from "styled-components";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
-  min-height: 100vh;
+  min-height: 94vh;
+  width: 100vw;
+`;
+export const Items = styled.div<{ smallscreen: boolean }>`
+  text-align: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: ${(props) => (props.smallscreen ? "75%" : "25%")};
+  > * {
+    margin: 10px 0;
+  }
+`;
+export const Input = styled.input`
+  width: 93%;
+  padding: 0.75rem;
+  margin-bottom: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #d9d9d9;
+`;
+export const SendreviewButton = styled.button`
+  width: 50%;
+  padding: 0.75rem;
+  margin-bottom: 1rem;
+  border-radius: 4px;
+  background-color: #78b2e7;
+  color: #fff;
+  border: none;
+  height: 40px;
+  &:hover {
+    background-color: #1d85e4;
+  }
+`;
+
+export const StyledIcon = styled(FontAwesomeIcon)`
+  color: #fcfcfd;
+  font-size: 24px;
+`;
+export const Textspan = styled.span`
+  color: #fcfcfd;
+  margin-left: 3px;
 `;
